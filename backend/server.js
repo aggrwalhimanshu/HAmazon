06456d823a0754d3c758157abfe509f2f1f28abb
agarwalhimanshu <aggrwalhimanshu@users.noreply.github.com>
2021-01-24 16:52:44 -0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLAIENT_ID || "sb");
 });
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
 
 // app.get("/", (req, res) => {
 //   res.send("Server is ready");
