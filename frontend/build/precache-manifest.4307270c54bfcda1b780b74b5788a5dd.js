3400ffb945139019a4b19fa7d0f3041517894704
agarwalhimanshu <aggrwalhimanshu@users.noreply.github.com>
2021-01-24 18:20:16 -0800

    ran build command and fixed dependencies

---
+++ frontend/build/precache-manifest.4307270c54bfcda1b780b74b5788a5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3f502accdd3888e0aecea19ad050aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b555562f9d33f3dbdbab",
+    "url": "/static/css/main.becab56d.chunk.css"
+  },
+  {
+    "revision": "3b2b8d6d3e66b404efd0",
+    "url": "/static/js/2.58cc1aee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.58cc1aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b555562f9d33f3dbdbab",
+    "url": "/static/js/main.ebc29bfb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
